e5154955281a7d6c3cbf838c069862b504c2af72
Ryan Werner <rmwerner@umich.edu>
2020-05-15 23:05:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// add solution here
+function theBeatlesPLay(musicians, instruments) {
+  var array = [];
+  for (let i = 0; i < 4; i++) {array.push(`${musicians[i]} plays ${instruments[i]}`)} 
+  return array}
+  
+  function johnLennonFacts(array) {
+    var newArray = []
+    while (i < array.length) {newArray.push(`${array[i]}!!!`); i++}
+    return newArray
+  }
+  
+  
